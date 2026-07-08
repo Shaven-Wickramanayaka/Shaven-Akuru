aa3d81e9253fc8e5824de4ab9a9222f9c90c0e5c
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-07-08 23:19:22 +0100

    Deployed onto Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shaven-wickramanayaka.github.io",
-  base: "/shaven-akuru",
+  base: "/Shaven-Akuru",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
