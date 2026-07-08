f47bf3674e4e5b23a92c0593c9eed23e43c0ea04
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-07-08 22:47:26 +0100

    added image urls in index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://shaven-wickramanayaka.github.io",
   base: "/Shaven-Akuru",
+  trailingSlash: "never",
   build: {
     assetsPrefix: "./",
   },
